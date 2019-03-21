bc8c2d8fa04843357401f8289d84b5a68cca4814
ycaokris <yi.cao2@mail.mcgill.ca>
2019-03-20 23:33:05 -0400

    dynamic generate list , and graph listen to list click

--- Data.py
+++ Data.py
@@ -1,35 +1,46 @@
 import pandas as pd
-import numpy as np
-import random
 
-column = ["Parameter", "Count", "Sparkline", "%OOC", "%OOC", "Pass / Fail"]
+df = pd.read_csv("data/spc_data.csv").dropna(axis=0)  # 481 * 19 cols
 
-df = pd.read_csv("data/wafer-production.csv")  # 481 * 19 cols
+#
+# def get_graph_trends(df, param, num):
+#     dff = df[['Batch', param]]
+#     stats = dff[param].describe()
+#
+#     count = stats['count'].tolist()
+#     std = stats['std'].tolist()
+#     mean = stats['mean'].tolist()
+#     ucl = (stats['mean'] + 3 * stats['std']).tolist()
+#     lcl = (stats['mean'] - 3 * stats['std']).tolist()
+#     min = stats['min'].tolist()
+#     max = stats['max'].tolist()
+#
+#     return dff, count, mean, ucl, lcl, min, max
 
 
+# Calculate OOC% for individual param
 def get_graph_trends(df, param):
-    dff = df[['Batch_Num', param]].dropna(axis=0)
-
-    print(dff)
+    dff = df[['Batch', param]][:]
     stats = dff[param].describe()
 
-    count = stats['count']
-    std = stats['std']
-    mean = stats['mean']
-    ucl = stats['mean'] + 3 * stats['std']
-    lcl = stats['mean'] - 3 * stats['std']
+    count = stats['count'].tolist()
 
-    # Spike in random outliers
-    sampl = np.random.uniform(low=-float(std), high=std, size=(40,))
-    rand_ind = np.random.randint(0, count, size=40)
-    print(sampl, rand_ind)
+    std = stats['std'].tolist()  # Process standard deviation
+    mean = stats['mean'].tolist()
+    ucl = (stats['mean'] + 3 * stats['std']).tolist()
+    lcl = (stats['mean'] - 3 * stats['std']).tolist()
+    min = stats['min'].tolist()
+    max = stats['max'].tolist()
 
-    for i in rand_ind:
-        dff[param][i] += 10
-
-    print('new dff:', dff)
-
-    return dff, count, mean, ucl, lcl
+    return dff, count, mean, ucl, lcl, min, max
 
 
-output = get_graph_trends(df, 'Metric1')
+output = get_graph_trends(df, 'Para8')
+print(output[1:])
+for i in output[0]['Para8']:
+    ooc = 0
+    if i > output[3] or i < output[4]:
+        print(i)
+        ooc += 1
+    ooc = (ooc/len(output[0]))*100
+print("ooc:", ooc, '%')
--- app.py
+++ app.py
@@ -1,35 +1,346 @@
+import os
+import sys
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
 
 from Data import *
 
 app = dash.Dash(__name__)
-server = app.server
+# server = app.server
 app.scripts.config.serve_locally = True
+app.config['suppress_callback_exceptions']=True
 
-df = pd.read_csv("data/wafer-production.csv").dropna()
+df = pd.read_csv("data/spc_data.csv")
+params = list(df)
 
-
-def generate_metrics_table():
-    table = html.Table(
-        id='Metric summary table',
+def generate_metric_row(id, col1, col2, col3, col4, col5, col6):
+    return html.Div(
+        id=id,
+        className='row metric-row',
         children=[
-            # Header
-            html.Tr(
-                id='table-header',
-                children=[html.Th(col) for col in column]),
-
-            # Body
-            html.Tr(
-                id='table-body',
-                children=[]
+            html.Div(
+                id=col1['id'],
+                style={},
+                className='one column',
+                children=col1['children']
+            ),
+            html.Div(
+                id=col2['id'],
+                style={},
+                className='one column',
+                children=col2['children']
+            ),
+            html.Div(
+                id=col3['id'],
+                style={},
+                className='five columns',
+                children=col3['children']
+            ),
+            html.Div(
+                id=col4['id'],
+                style={},
+                className='one column',
+                children=col4['children']
+            ),
+            html.Div(
+                id=col5['id'],
+                style={},
+                className='three columns',
+                children=col5['children']
+            ),
+            html.Div(
+                id=col6['id'],
+                style={},
+                className='one column',
+                children=col6['children']
             )
         ]
     )
-    return table
+
+
+def create_callback(retfunc):
+    """
+    pass *input_value to retfunc
+
+    creates a callback function
+    """
+    def callback(*input_values):
+        if input_values is not None and input_values != 'None':
+            try:
+                retval = retfunc(*input_values)
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print('Callback Exception:', e, exc_type, fname, exc_tb.tb_lineno)
+                print('parameters:', *input_values)
+            return retval
+        else:
+            return []
+
+    return callback
+
+
+def generate_graph(interval, value, curr_fig):
+    dff, count, mean, ucl, lcl, min, max = get_graph_trends(df, value)
+
+    if curr_fig['data'][0]['name'] != value:
+        curr_fig['data'][0]['y'] = []
+        curr_fig['data'][0]['x'] = []
+
+    layout = dict(title='Individual measurements', showlegend=True, xaxis={
+        'zeroline': False,
+        'title': 'Batch_Num',
+        'showline': False
+    }, yaxis={
+        'title': value,
+        'autorange': True
+    }, annotations=[
+        {'x': 2, 'y': lcl, 'xref': 'x', 'yref': 'y', 'text': 'LCL', 'showarrow': True},
+    ], shapes=[
+        {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'y',
+            'x0': 1,
+            'y0': ucl,
+            'x1': interval + 2,
+            'y1': ucl,
+            'line': {
+                'color': 'rgb(50, 171, 96)',
+                'width': 1,
+                'dash': 'dashdot'
+            }
+        },
+        {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'y',
+            'x0': 1,
+            'y0': mean,
+            'x1': interval + 2,
+            'y1': mean,
+            'line': {
+                'color': 'rgb(255,127,80)',
+                'width': 2
+            }
+        },
+        {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'y',
+            'x0': 1,
+            'y0': lcl,
+            'x1': interval + 2,
+            'y1': lcl,
+            'line': {
+                'color': 'rgb(50, 171, 96)',
+                'width': 1,
+                'dash': 'dashdot'
+            }
+        }
+    ])
+
+    x_array = dff['Batch'].tolist()
+    y_array = dff[value].tolist()
+
+    curr_fig['data'][0]['name'] = value
+
+    if len(curr_fig['data'][0]['x']) < count:
+        curr_fig['data'][0]['x'].append(x_array[len(curr_fig['data'][0]['x'])])
+        curr_fig['data'][0]['y'].append(y_array[len(curr_fig['data'][0]['y'])])
+        curr_fig['layout'] = layout
+
+    return curr_fig
+
+
+def generate_metric_list():
+
+    # 1 build header
+    metric_header_div = [generate_metric_row('metric_header',  {
+            'id': "m_header_1",
+            'children': html.Div("Parameter")
+        },
+        {
+            'id': "m_header_2",
+            'children': html.Div("Count")
+        },
+        {
+            'id': "m_header_3",
+            'children': html.Div("Sparkline")
+        },
+        {
+            'id': "m_header_4",
+            'children': html.Div("%OOC")
+        },
+        {
+            'id': "m_header_5",
+            'children': html.Div("%OOC")
+        },
+        {
+            'id': "m_header_6",
+            'children': html.Div("Pass / Fail")
+        })]
+    input_list = []
+    for index in range(1, len(params)):
+        item = params[index]
+        input_list.append(Input(item, 'n_clicks'))
+        metric_header_div.append(
+            generate_metric_row(
+                item,
+                {
+                    'id': item+"_"+str(index),
+                    'children': item
+                },
+                {
+                    'id': item+'_count_'+str(index),
+                    'children': 0
+                },
+                {
+                    'id': item+'_sparkline_'+str(index),
+                    'children': html.Div("aaa")
+                },
+                {
+                    'id': item+'_OCCnumber_'+str(index),
+                    'children': 0
+                },
+                {
+                    'id': item+'_OCCgraph_'+str(index),
+                    'children': html.Div('aaa')
+                },
+                {
+                    'id': item+'_pf_'+str(index),
+                    'children': html.Div('aaa')
+                }
+            )
+        )
+
+    def update_graph(*inputs):
+        click_state = inputs[-1]
+        interval = inputs[-3]
+        figure = inputs[-2]
+
+        if len(click_state) == 0:
+            return generate_graph(interval, params[8], figure), inputs[:-3]
+        for j in range(len(inputs) - 3):
+            if click_state[j] != inputs[j]:
+                return generate_graph(interval, params[j+1], figure), inputs[:-3]
+
+        curr_fig = figure['data'][0]['name']
+        return generate_graph(interval, curr_fig, figure), inputs[:-3]
+
+    input_list.append(Input('interval-component', 'n_intervals'))
+    # app referencing the dash app object
+    app.callback(output=[Output('control-chart-live', 'figure'), Output('click_state', 'data')],
+                 inputs=input_list,
+                 state=[State('control-chart-live', 'figure'), State('click_state', 'data')]
+                 )(create_callback(update_graph))
+
+    return html.Div(
+        id='metric_list',
+        className='row',
+        style={
+            'overflow': 'scroll',
+            'height': '100%'
+        },
+        children=metric_header_div
+    )
+    # for each row, build a div
+
+
+def generate_tree_map():
+    return html.Div(
+        children=[
+            html.Label(
+                id='test_label',
+                children="0"
+            ),
+            dcc.Store(
+                id='click_state',
+                data=[]
+            ),
+        ])
+
+
+
+def build_top_panel():
+    return html.Div(
+        id='top-section-container',
+        className='row',
+        style={
+            'height': '45vh'
+        },
+        children=[
+            # Metrics summary
+            html.Div(
+                id='metric-summary-session',
+                className='six columns',
+                style={'height':'100%'},
+                children=[
+                    generate_section_banner('Process Control Metrics Summary'),
+                    html.Div(
+                        id='metric_div',
+                        style={
+                            'height': '100%',
+                            'width': '100%',
+                        },
+                        children=generate_metric_list()
+                    )
+                ]
+            ),
+            # Tree Map
+            html.Div(
+                id='treemap-session',
+                className='six columns',
+                children=[
+                    generate_section_banner('% OOC per Parameter'),
+                    generate_tree_map()
+                ]
+            )
+        ]
+    )
+
+
+def build_chart_panel():
+    return html.Div(
+        id='control-chart-container',
+        className='twelve columns',
+        children=[
+            generate_section_banner('Live SPC Chart'),
+
+            dcc.Dropdown(
+                # Select for live-stream figure display
+                id='dropdown-select',
+                options=list({'label': i, 'value': i} for i in params),
+                value="Para4"
+            ),
+
+            dcc.Interval(
+                id='interval-component',
+                interval=1 * 1000,  # in milliseconds
+                n_intervals=0
+            ),
+
+            dcc.Graph(
+                id="control-chart-live",
+                figure=go.Figure({
+                    'data': [{'x': [], 'y': [], 'mode': 'lines+markers', 'name': 'Para4'}]
+                }
+                )
+            ),
+
+            dcc.Graph(
+                id="moving-range",
+                figure=go.Figure({
+                    'data': [{'x': [], 'y': [], 'mode': 'lines+markers'}]
+                }
+                )
+            )
+        ])
 
 
 def generate_section_banner(title):
@@ -72,103 +383,95 @@ app.layout = html.Div(
             id='tabs-content',
             className='container scalable',
             children=[
-                html.Div(
-                    id='top-section-container',
-                    className='row',
-                    children=[
-                        # Metrics summary
-                        html.Div(
-                            id='metric-summary-session',
-                            className='six columns',
-                            children=[
-                                generate_section_banner('Process Control Metrics Summary'),
-                                generate_metrics_table()
-                            ]
-                        ),
-                        # Treemap
-                        html.Div(
-                            id='treemap-session',
-                            className='six columns',
-                            children=generate_section_banner('% OOC per Parameter')
-                        )
-                    ]
-                ),
+                build_top_panel(),
+                build_chart_panel(),
                 # Control chart
-                html.Div(
-                    id='control-chart-container',
-                    className='twelve columns',
-                    children=[
-                        generate_section_banner('Control Chart'),
-
-                        dcc.Interval(
-                            id='interval-component',
-                            interval=1 * 1000,  # in milliseconds
-                            n_intervals=0
-                        ),
-
-                        dcc.Graph(
-                            id="control-chart-live",
-                            figure=go.Figure({
-                                'data': [{'x': [], 'y': []}]
-                            })
-                        )
-                    ]
-                )
             ]
         )
     ]
 )
 
 
-@app.callback(
-    Output('control-chart-live', 'figure'),
-    [Input('interval-component', 'n_intervals')],
-    state=[State('control-chart-live', 'figure')]
-)
-def update_chart(interval, curr_fig):
-    dff, stats, count, mean, ucl, lcl = get_graph_trendline(df, 'Length1')
-    layout = {
-        'shapes':
-            [
-                {
-                    'type': 'line',
-                    'x0': 1,
-                    'y0': ucl,
-                    'x1': count,
-                    'y1': ucl,
-                    'line': {
-                        'color': 'rgb(50, 171, 96)',
-                        'width': 2,
-                        'dash': 'dashdot'
-                    }
-                },
+# Live SPC updates by Batch_Num
+# @app.callback(
+#     Output('control-chart-live', 'figure'),
+#     [Input('interval-component', 'n_intervals'),
+#      Input('dropdown-select', 'value')],
+#     state=[State('control-chart-live', 'figure')]
+# )
+def update_chart(interval, value, curr_fig):
+    dff, count, mean, ucl, lcl, min, max = get_graph_trends(df, value)
 
-                {
-                    'type': 'line',
-                    'x0': 1,
-                    'y0': lcl,
-                    'x1': count,
-                    'y1': lcl,
-                    'line': {
-                        'color': 'rgb(50, 171, 96)',
-                        'width': 2,
-                        'dash': 'dashdot'
-                    }
-                }]
-    }
+    if curr_fig['data'][0]['name'] != value:
+        curr_fig['data'][0]['y'] = []
+        curr_fig['data'][0]['x'] = []
 
-    x_array = dff['Batch_Num'].tolist()
-    y_array = dff['Length1'].tolist()
+    layout = dict(title='Individual measurements', showlegend=True, xaxis={
+        'zeroline': False,
+        'title': 'Batch_Num',
+        'showline': False
+    }, yaxis={
+        'title': value,
+        'autorange': True
+    }, annotations=[
+        {'x': 2, 'y': lcl, 'xref': 'x', 'yref': 'y', 'text': 'LCL', 'showarrow': True},
+    ], shapes=[
+        {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'y',
+            'x0': 1,
+            'y0': ucl,
+            'x1': interval + 2,
+            'y1': ucl,
+            'line': {
+                'color': 'rgb(50, 171, 96)',
+                'width': 1,
+                'dash': 'dashdot'
+            }
+        },
+        {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'y',
+            'x0': 1,
+            'y0': mean,
+            'x1': interval + 2,
+            'y1': mean,
+            'line': {
+                'color': 'rgb(255,127,80)',
+                'width': 2
+            }
+        },
+        {
+            'type': 'line',
+            'xref': 'x',
+            'yref': 'y',
+            'x0': 1,
+            'y0': lcl,
+            'x1': interval + 2,
+            'y1': lcl,
+            'line': {
+                'color': 'rgb(50, 171, 96)',
+                'width': 1,
+                'dash': 'dashdot'
+            }
+        }
+    ])
 
-    curr_fig['data'][0]['x'].append(x_array[interval])
-    curr_fig['data'][0]['y'].append(y_array[interval])
+    x_array = dff['Batch'].tolist()
+    y_array = dff[value].tolist()
 
-    curr_fig['layout'] = layout
+    curr_fig['data'][0]['name'] = value
+
+    if len(curr_fig['data'][0]['x']) < count:
+        curr_fig['data'][0]['x'].append(x_array[len(curr_fig['data'][0]['x'])])
+        curr_fig['data'][0]['y'].append(y_array[len(curr_fig['data'][0]['y'])])
+        curr_fig['layout'] = layout
 
     return curr_fig
 
 
 # Running the server
 if __name__ == '__main__':
-    app.run_server(dev_tools_hot_reload=False, debug=True, host='0.0.0.0', port=8051)
-
+    app.run_server(dev_tools_hot_reload=False, debug=True, host='0.0.0.0', port=8051, use_reloader=False)
